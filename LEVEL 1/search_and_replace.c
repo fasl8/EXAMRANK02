1803250e8c4edfb28ffd06147df42aa18d493d1b
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-03 20:24:45 +0400

    Update search_and_replace.c

--- LEVEL 1/search_and_replace.c
+++ LEVEL 1/search_and_replace.c
@@ -29,20 +29,22 @@ eNcOre Un ExEmPle Pas Facile a Ecrire $
 
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int i = 0;
+	int	i;
 
-    if (argc == 4 && !argv[2][1] && argv[3][1])
-    {
-        while (argv[1][i])
-        {
-            if (argv[1][i] == argv[2][0])
-				write(1, &argv[3][0], 1);
-			else 
-					write(1, &argv[1][i], 1);
-				i++;
-        }
-    }
+	i = 0;
+	if (argc == 4 && !argv[2][1] && !argv[3][1])
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] == argv[2][0])
+			{
+				argv[1][i] = argv[3][0];
+			}
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
 	write(1, "\n", 1);
 }

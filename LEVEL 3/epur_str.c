80d43b09c1df1cb328f1bea2e83ed31046620445
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-03 21:59:16 +0400

    Add files via upload

---
+++ LEVEL 3/epur_str.c
@@ -0,0 +1,68 @@
+// Assignment name  : epur_str
+// Expected files   : epur_str.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a string,
+	// and displays this string with exactly one
+// space between words,
+	// with no spaces or tabs either at the beginning or the end,
+// followed by a \n.
+
+// A "word" is defined as a part of a string delimited either by spaces/tabs, or
+// by the start/end of the string.
+
+// If the number of arguments is not 1, or if there are no words to display, the
+// program displays \n.
+
+// Example:
+
+// $> ./epur_str "See? It's easy to print the same thing" | cat -e
+// See? It's easy to print the same thing$
+// $> ./epur_str " this        time it      will     be    more complex  . " | cat
+	// -e
+// this time it will be more complex .$
+// $> ./epur_str "No S*** Sherlock..." "nAw S*** ShErLaWQ..." | cat -e
+// $
+// $> ./epur_str "" | cat -e
+// $
+// $>
+
+// $> ./epur_str "vous voyez c'est facile d'afficher la meme chose" | cat -e
+// vous voyez c'est facile d'afficher la meme chose$
+// $> ./epur_str " seulement          la c'est      plus dur " | cat -e
+// seulement la c'est plus dur$
+// $> ./epur_str "comme c'est cocasse" "vous avez entendu, Mathilde ?" | cat -e
+// $
+// $> ./epur_str "" | cat -e
+// $
+// $>
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	flag;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i++;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				flag = 1;
+			if (argv[1][i] != ' ' && argv[1][i] != '\t')
+			{
+				if (flag)
+					write(1, " ", 1);
+				flag = 0;
+				write(1, &argv[1][i], 1);
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}

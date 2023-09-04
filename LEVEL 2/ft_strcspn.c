3d6cfc34fcd6476d0cfa31fc11357c76d38a2670
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-04 19:53:07 +0400

    Add files via upload

---
+++ LEVEL 2/ft_strcspn.c
@@ -0,0 +1,41 @@
+// Assignment name	: ft_strcspn
+// Expected files	: ft_strcspn.c
+// Allowed functions: None
+// ---------------------------------------------------------------
+
+// Reproduce exactly the behavior of the function strcspn
+// (man strcspn).
+
+// The function should be prototyped as follows:
+
+// size_t	ft_strcspn(const char *s, const char *reject);
+
+#include <stddef.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (reject[b] != '\0')
+		{
+			if (s[a] == reject[b])
+				return (a);
+			b++;
+		}
+		a++;
+	}
+	return (a);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	printf("%zu", ft_strcspn("s", "r"));
+// }

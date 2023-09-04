82db4fd874581180f323093879b08504a1d749b5
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-04 19:58:20 +0400

    Add files via upload

--- LEVEL 1/rotone.c
+++ LEVEL 1/rotone.c
@@ -1,16 +1,46 @@
+// Assignment name  : rotone
+// Expected files   : rotone.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a string and displays it, replacing each of its
+// letters by the next one in alphabetical order.
+
+// 'z' becomes 'a' and 'Z' becomes 'A'. Case remains unaffected.
+
+// The output will be followed by a \n.
+
+// If the number of arguments is not 1, the program displays \n.
+
+// Example:
+
+// $>./rotone "abc"
+// bcd
+// $>./rotone "Les stagiaires du staff ne sentent pas toujours tres bon." | cat -e
+// Mft tubhjbjsft ev tubgg of tfoufou qbt upvkpvst usft cpo.$
+// $>./rotone "AkjhZ zLKIJz , 23y " | cat -e
+// BlkiA aMLJKa , 23z $
+// $>./rotone | cat -e
+// $
+// $>
+// $>./rotone "" | cat -e
+// $
+// $>
+
 #include <unistd.h>
-int main (int argc, char **argv)
+
+int	main(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
-			if ((argv[1][i] >= 'a' && argv[1][i] <= 'y') || (argv[1][i] >= 'A' && argv[1][i] <= 'Y'))
+			if ((argv[1][i] >= 'A' && argv[1][i] <= 'Y') || (argv[1][i] >= 'a' && argv[1][i] <= 'y'))
 				argv[1][i] += 1;
-			else if (argv[1][i] == 'Z' || argv[1][i] == 'z')
+			else if (argv[1][i] >= 'Z' && argv[1][i] <= 'z')
 				argv[1][i] -= 25;
 			write(1, &argv[1][i], 1);
 			i++;
@@ -18,3 +48,5 @@ int main (int argc, char **argv)
 	}
 	write(1, "\n", 1);
 }
+
+//if = a->y +=1, Z->z -=25  

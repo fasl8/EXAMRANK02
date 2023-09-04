bf056ef4f8037ee389dae248b78fbc6bc2fd5617
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-04 19:55:11 +0400

    Add files via upload

---
+++ LEVEL 1/ft_strcpy.c
@@ -0,0 +1,24 @@
+// Assignment name  : ft_strcpy
+// Expected files   : ft_strcpy.c
+// Allowed functions: 
+// --------------------------------------------------------------------------------
+
+// Reproduce the behavior of the function strcpy (man strcpy).
+
+// Your function must be declared as follows:
+
+// char    *ft_strcpy(char *s1, char *s2);
+
+char	*ft_strcpy(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s2[i])
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}

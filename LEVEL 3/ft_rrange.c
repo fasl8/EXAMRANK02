2f82c71dd6c9a1b4bf641ebdea0e0e9e2176651a
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-04 19:48:03 +0400

    Add files via upload

--- LEVEL 3/ft_rrange.c
+++ LEVEL 3/ft_rrange.c
@@ -7,7 +7,8 @@
 
 // int     *ft_rrange(int start, int end);
 
-// It must allocate (with malloc()) an array of integers, fill it with consecutive
+// It must allocate (with malloc()) an array of integers,
+// fill it with consecutive
 // values that begin at end and end at start (Including start and end !), then
 // return a pointer to the first value of the array.
 
@@ -17,7 +18,6 @@
 // - With (-1, 2) you will return an array containing 2, 1, 0 and -1.
 // - With (0, 0) you will return an array containing 0.
 // - With (0, -3) you will return an array containing -3, -2, -1 and 0.
-
 #include <stdlib.h>
 
 int	*ft_rrange(int start, int end)
@@ -26,8 +26,8 @@ int	*ft_rrange(int start, int end)
 	int	i;
 	int	n;
 
-	n = end - start + i;
 	i = 0;
+	n = end - start + 1;
 	if (start > end)
 		return (ft_rrange(end, start));
 	range = (int *)malloc(sizeof(int) * n);
@@ -42,3 +42,26 @@ int	*ft_rrange(int start, int end)
 	}
 	return (range);
 }
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	int	*tab;
+// 	int	index;
+// 	int	start;
+// 	int	end;
+// 	int	size;
+
+// 	index = 0;
+// 	start = -1;
+// 	end = 2;
+// 	tab = ft_rrange(start, end);
+// 	size = abs(end - start) + 1;
+// 	while (index < size)
+// 	{
+// 		printf(" [%i] ", tab[index]);
+// 		index++;
+// 	}
+// 	printf("\n");
+// }

99b4ac8597dc93c66241db89e330ec492fa86404
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-06 18:56:47 +0400

    Add files via upload

---
+++ LEVEL 3/ft_range.c
@@ -0,0 +1,71 @@
+// Assignment name  : ft_range
+// Expected files   : ft_range.c
+// Allowed functions: malloc
+// --------------------------------------------------------------------------------
+
+// Write the following function:
+
+// int     *ft_range(int start, int end);
+
+// It must allocate (with malloc()) an array of integers, fill it with consecutive
+// values that begin at start and end at end (Including start and end !), then
+// return a pointer to the first value of the array.
+
+// Examples:
+
+// - With (1, 3) you will return an array containing 1, 2 and 3.
+// - With (-1, 2) you will return an array containing -1, 0, 1 and 2.
+// - With (0, 0) you will return an array containing 0.
+// - With (0, -3) you will return an array containing 0, -1, -2 and -3.
+
+// #include <stdlib.h>
+
+// int *ft_range(int start, int end)
+// {
+// 	int i = 0;
+// 	int len = abs((end - start)) + 1;
+// 	int *res = (int *)malloc(sizeof(int) * len);
+	
+// 	while (i < len)
+// 	{
+// 		if (start < end)
+// 		{
+// 			res[i] = start;
+// 			start++;
+// 			i++;
+// 		}
+// 		else
+// 		{
+// 			res[i] = start;
+// 			start--;
+// 			i++;
+// 		}
+// 	}
+//         return (res);
+// }
+
+#include <stdlib.h>
+
+int *ft_range(int start, int end)
+{
+    int *tab;
+    int len;
+
+    if (start > end)
+        len = start - end;
+    else
+        len = end - start;
+    tab = malloc(sizeof(int) * len + 1);
+    if (!tab)
+        return (NULL);
+    while (len >= 0)
+    {
+        tab[len] = end;
+        if (start > end)
+            end++;
+        else
+            end--;
+        len--;
+    }
+    return (tab);
+}

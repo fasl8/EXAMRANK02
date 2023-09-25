a79ff9123ad2498d06d77a6270876c25f8f04a49
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-25 16:46:29 +0400

    Add files via upload

--- LEVEL 1/ulstr.c
+++ LEVEL 1/ulstr.c
@@ -1,10 +1,36 @@
+// Assignment name  : ulstr
+// Expected files   : ulstr.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that takes a string and reverses the case of all its letters.
+// Other characters remain unchanged.
+
+// You must display the result followed by a '\n'.
+
+// If the number of arguments is not 1, the program displays '\n'.
+
+// Examples :
+
+// $>./ulstr "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification." | cat -e
+// l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$
+// $>./ulstr "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  " | cat -e
+// s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $
+// $>./ulstr "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot" | cat -e
+// 3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$
+// $>./ulstr | cat -e
+// $
+
 #include <unistd.h>
-int main(int argc, char **argv)
+
+int	main(int argc, char **argv)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] != '\0')
+		while (argv[1][i])
 		{
 			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
 				argv[1][i] += 32;

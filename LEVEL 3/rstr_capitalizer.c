77ac5d37b99ead40fe9106d391a65911bbcc5111
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-25 16:50:08 +0400

    Add files via upload

--- LEVEL 3/rstr_capitalizer.c
+++ LEVEL 3/rstr_capitalizer.c
@@ -3,7 +3,7 @@
 // Allowed functions: write
 // --------------------------------------------------------------------------------
 
-// Write a program that takes one or more strings and, for each argument, puts
+// Write a program that takes one or more strings and, for each argument, puts 
 // the last character that is a letter of each word in uppercase and the rest
 // in lowercase, then displays the result followed by a \n.
 
@@ -20,12 +20,11 @@
 // $
 // $> ./rstr_capitalizer "a FiRSt LiTTlE TESt" | cat -e
 // A firsT littlE tesT$
-// $> ./rstr_capitalizer "SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay,
-// 	this is the last 1239809147801 but not    the least    t" | cat -e
-// // seconD tesT A littlE biT   moaR compleX$
-// //    but... thiS iS noT thaT compleX$
-// //      okay, thiS iS thE lasT 1239809147801 buT noT    thE leasT    T$
-// // $>
+// $> ./rstr_capitalizer "SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t" | cat -e
+// seconD tesT A littlE biT   moaR compleX$
+//    but... thiS iS noT thaT compleX$
+//      okay, thiS iS thE lasT 1239809147801 buT noT    thE leasT    T$
+// $>
 
 #include <unistd.h>
 
@@ -58,7 +57,7 @@ int	main(int argc, char **argv)
 		{
 			rstr_capitalizer(argv[i]);
 			write(1, "\n", 1);
-			i += 1;
+			i++;
 		}
 	}
 	return (0);

de2661f3e805c9fee6c0b64e451675f7e39d89d6
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-09-27 20:48:31 +0400

    Create ft_strspn.c

---
+++ LEVEL 2/ft_strspn.c
@@ -0,0 +1,37 @@
+// Assignment name	: ft_strspn
+// Expected files	: ft_strspn.c
+// Allowed functions: None
+// ---------------------------------------------------------------
+
+// Reproduce exactly the behavior of the strspn function 
+// (man strspn).
+
+// The function should be prototyped as follows:
+
+// size_t	ft_strspn(const char *s, const char *accept);
+
+#include <stdlib.h>
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	int i;
+	int	j;
+	int check;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		check = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+				check = 1;
+			j++;
+		}
+		if (check == 0)
+			return (i);
+		i++;
+	}
+	return (i);
+}

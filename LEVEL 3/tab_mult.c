3ead9ee609928444545a01cd5209288864be1daf
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-10-05 10:35:32 +0400

    Add files via upload

---
+++ LEVEL 3/tab_mult.c
@@ -0,0 +1,97 @@
+// Assignment name  : tab_mult
+// Expected files   : tab_mult.c
+// Allowed functions: write
+// --------------------------------------------------------------------------------
+
+// Write a program that displays a number's multiplication table.
+
+// The parameter will always be a strictly positive number that fits in an int,
+// and said number times 9 will also fit in an int.
+
+// If there are no parameters, the program displays \n.
+
+// Examples:
+
+// $>./tab_mult 9
+// 1 x 9 = 9
+// 2 x 9 = 18
+// 3 x 9 = 27
+// 4 x 9 = 36
+// 5 x 9 = 45
+// 6 x 9 = 54
+// 7 x 9 = 63
+// 8 x 9 = 72
+// 9 x 9 = 81
+// $>./tab_mult 19
+// 1 x 19 = 19
+// 2 x 19 = 38
+// 3 x 19 = 57
+// 4 x 19 = 76
+// 5 x 19 = 95
+// 6 x 19 = 114
+// 7 x 19 = 133
+// 8 x 19 = 152
+// 9 x 19 = 171
+// $>
+// $>./tab_mult | cat -e
+// $
+// $>
+
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{
+	int result;
+	int sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
+
+void	ft_putnbr(int n)
+{
+	char digit;
+
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	digit = (n % 10) + '0';
+	write(1, &digit, 1);
+}
+
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	nbr;
+
+	if (argc != 2)
+		write(1, "\n", 1);
+	else
+	{
+		i = 1;
+		nbr = ft_atoi(argv[1]);
+		while (i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(nbr);
+			write(1, " = ", 3);
+			ft_putnbr(i * nbr);
+			write(1, "\n", 1);
+			i += 1;
+		}
+	}
+	return (0);
+}

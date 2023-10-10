c0b33e6fa6878a38515631dde9989887dcf5d99b
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-10-10 15:03:53 +0400

    Update print_hex.c

--- LEVEL 3/print_hex.c
+++ LEVEL 3/print_hex.c
@@ -23,27 +23,27 @@
 
 int ft_atoi(char *str)
 {
-    int result;
+	int result;
 
-    while (*str != '\0')
-        result = result * 10 + *str++ - '0';
-    return (result);
+	result = 0;
+	while (*str != '\0')
+		result = result * 10 + *str++ - '0';
+	return (result);
 }
 
-int print_hex(int nbr)
+void print_hex(int nbr)
 {
-    char hex[] = "0123456789abcdef";
+	char hex[] = "0123456789abcdef";
 
-    if (nbr >= 16)
-        print_hex(nbr / 16);
-    write(1, &hex[nbr % 16], 1);
+	if (nbr >= 16)
+		print_hex(nbr / 16);
+	write(1, &hex[nbr % 16], 1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc == 2)
-    {
-        print_hex(ft_atoi(argv[1]));
-    }
-    write(1, "\n", 1);
+
+	if (argc == 2)
+		print_hex(ft_atoi(argv[1]));
+	write(1, "\n", 1);
 }

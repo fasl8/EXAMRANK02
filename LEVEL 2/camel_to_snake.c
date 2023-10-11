597e0e762400574a86c4323cffde216037e785f8
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-10-11 13:50:11 +0400

    Update camel_to_snake.c

--- LEVEL 2/camel_to_snake.c
+++ LEVEL 2/camel_to_snake.c
@@ -22,19 +22,22 @@
 
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
-			if (i != 0 && argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
 			{
-				write(1, "_", 1);
-				argv[1][i] += 32;
+				if (i > 0)
+				{
+					argv[1][i] = argv[1][i] + ' ';
+					write(1, "_", 1);
+				}
 			}
 			write(1, &argv[1][i], 1);
 			i++;

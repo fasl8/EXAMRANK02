50eb07b11f07d2da94072818bd34f4925c5e192f
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-10-16 14:46:02 +0400

    Update rostring.c

--- LEVEL 4/rostring.c
+++ LEVEL 4/rostring.c
@@ -55,7 +55,7 @@ int	main(int argc, char **argv)
 			while ((argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t') 
 				&& (argv[1][i - 1] == ' ' || argv[1][i - 1] == '\t'))
 			{
-				while (argv[1][i] != ' ' && argv[1][i] != '\t' && argv[1][i] != '\0')
+				while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
 				{
 					write(1, &argv[1][i], 1);
 					i++;

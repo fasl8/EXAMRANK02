b487d9b72f177dac07149d91aae0d3458680771a
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-10-31 20:32:35 +0400

    Update ft_atoi_base.c

--- LEVEL 3/ft_atoi_base.c
+++ LEVEL 3/ft_atoi_base.c
@@ -19,49 +19,84 @@
 
 // int	ft_atoi_base(const char *str, int str_base);
 
-char	to_lower(char c)
+// char	to_lower(char c)
+// {
+// 	if (c >= 'A' && c <= 'Z')
+// 		return (c + 32);
+// 	return (c);
+// }
+
+// int	get_digit(char c, int digits_in_base)
+// {
+// 	int	max_digit;
+
+// 	if (digits_in_base <= 10)
+// 		max_digit = digits_in_base + '0';
+// 	else
+// 		max_digit = digits_in_base - 10 + 'a';
+// 	if (c >= '0' && c <= '9' && c <= max_digit)
+// 		return (c - '0');
+// 	else if (c >= 'a' && c <= 'f' && c <= max_digit)
+// 		return (10 + c - 'a');
+// 	else
+// 		return (-1);
+// }
+
+// int	ft_atoi_base(const char *str, int str_base)
+// {
+// 	int	result;
+// 	int	sign;
+// 	int	digit;
+
+// 	result = 0;
+// 	sign = 1;
+// 	if (*str == '-')
+// 	{
+// 		sign = -1;
+// 		++str;
+// 	}
+// 	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
+// 	{
+// 		result = result * str_base;
+// 		result = result + (digit * sign);
+// 		++str;
+// 	}
+// 	return (result);
+// }
+int ft_atoi_base(const char *str, int str_base)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (c + 32);
-	return (c);
-}
+	int nb = 0;
+	int i = 0;
+	int sign = 1;
 
-int	get_digit(char c, int digits_in_base)
-{
-	int	max_digit;
-
-	if (digits_in_base <= 10)
-		max_digit = digits_in_base + '0';
-	else
-		max_digit = digits_in_base - 10 + 'a';
-	if (c >= '0' && c <= '9' && c <= max_digit)
-		return (c - '0');
-	else if (c >= 'a' && c <= 'f' && c <= max_digit)
-		return (10 + c - 'a');
-	else
-		return (-1);
-}
-
-int	ft_atoi_base(const char *str, int str_base)
-{
-	int	result;
-	int	sign;
-	int	digit;
-
-	result = 0;
-	sign = 1;
-	if (*str == '-')
+	while(str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[0] == '-')
 	{
 		sign = -1;
-		++str;
+		i++;
 	}
-	while ((digit = get_digit(to_lower(*str), str_base)) >= 0)
+	else if (str[0] == '+')
+		i++;
+	while (((str[i] >= '0' && str[i] <= '9') && (str_base <= 16)) || ((str[i] >= 'a' && str[i] <= 'f') || (str[i] >= 'A' && str[i] <= 'F')))
 	{
-		result = result * str_base;
-		result = result + (digit * sign);
-		++str;
+		nb = nb * str_base;
+		if (str[i] >= '0' && str[i] <= '9')
+			nb = nb + str[i] - '0';
+		else if (str[i] >= 'a' && str[i] <= 'f')
+			nb = nb + str[i] - 'a' + 10;
+		else if (str[i] >= 'A' && str[i] <= 'F')
+			nb = nb + str[i] - 'A' + 10;
+		i++;
 	}
-	return (result);
+	return (nb * sign);
+}
+//GRADEME SUCCESS
+#include<stdio.h>
+int main()
+{
+       printf("%d\n", ft_atoi_base("ABCHIJ" ,16));
+    return 0;
 }
 
 #include <stdio.h>

fe38380dcca796bf5d9755bd1bf7ffe28b1ea46d
fasl8 <141274071+fasl8@users.noreply.github.com>
2023-11-13 00:46:10 +0400

    Add files via upload

---
+++ LEVEL 4/sort_int_tab.c
@@ -0,0 +1,34 @@
+// Assignment name  : sort_int_tab
+// Expected files   : sort_int_tab.c
+// Allowed functions:
+// --------------------------------------------------------------------------------
+
+// Write the following function:
+
+// void sort_int_tab(int *tab, unsigned int size);
+
+// It must sort (in-place) the 'tab' int array, that contains exactly 'size'
+// members, in ascending order.
+
+// Doubles must be preserved.
+
+// Input is always coherent.
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i = 0;
+	int	temp;
+
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i+ 1];
+			tab[i + 1] = temp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
